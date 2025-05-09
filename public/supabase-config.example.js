e53641818323126b4407659e47413efff7bfe686
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2025-05-10 03:31:19 +0530

    fixed bugs and erros

--- public/supabase-config.example.js
+++
@@ -1,7 +0,0 @@
-// This is just an example file of how the creds in the original file should look like
-// you might wanna add the creds by going to your supabase settings
-
-
-// supabase-config.example.js
-export const SUPABASE_URL = 'https://your-project.supabase.co';
-export const SUPABASE_ANON_KEY = 'your-anon-key';
